c5f4ad9d2efd1d099fa7ccd1ac809a82697b6b9a
Larissa Cruz <larissacruz_01@hotmail.com>
2023-08-12 17:03:44 -0300

    criacao do projeto spring boot

---
+++ leiloes-api/src/main/java/br/com/leilao/leiloesapi/LeiloesApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.leilao.leiloesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeiloesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeiloesApiApplication.class, args);
+	}
+
+}
