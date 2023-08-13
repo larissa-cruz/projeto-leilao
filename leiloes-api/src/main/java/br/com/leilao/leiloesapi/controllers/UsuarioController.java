63e4a69a74debe68a17f3492dbeefee3fc0ddea8
Larissa Cruz <larissacruz_01@hotmail.com>
2023-08-13 20:59:17 -0300

    criando tabela usuarios e propriedade JPA

--- leiloes-api/src/main/java/br/com/leilao/leiloesapi/controllers/UsuarioController.java
+++ leiloes-api/src/main/java/br/com/leilao/leiloesapi/controllers/UsuarioController.java
@@ -1,19 +1,27 @@
 package br.com.leilao.leiloesapi.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.leilao.leiloesapi.usuario.DadosCadastroUsuario;
+import br.com.leilao.leiloesapi.usuario.Usuario;
+import br.com.leilao.leiloesapi.usuario.UsuarioRepository;
 
 @RestController
-@RequestMapping("cadastrousuario")
+@RequestMapping("/cadastrousuario")
 public class UsuarioController {
 
-    @PostMapping
-    public void cadastrarUsuario(@RequestBody DadosCadastroUsuario dadosCadastroUsuario) {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
+    @PostMapping
+    @Transactional
+    public void cadastrarUsuario(@RequestBody DadosCadastroUsuario dadosCadastroUsuario) {
+        usuarioRepository.save(new Usuario(dadosCadastroUsuario));
     }
     
 }
