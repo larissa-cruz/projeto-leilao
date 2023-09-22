f3169c567b7e2793f2387196567cf3aa00123348
Larissa Cruz <larissacruz_01@hotmail.com>
2023-09-22 20:00:59 -0300

    alterando dados listagem leilao

--- leiloes-api/src/main/java/br/com/leilao/leiloesapi/leilao/DadosListagemLeilao.java
+++ leiloes-api/src/main/java/br/com/leilao/leiloesapi/leilao/DadosListagemLeilao.java
@@ -2,10 +2,11 @@ package br.com.leilao.leiloesapi.leilao;
 
 import java.time.LocalDate;
 
-public record DadosListagemLeilao(String name, Double price, LocalDate data) {
+
+public record DadosListagemLeilao(String name, Double price, LocalDate data, Long id, Long idusuario) {
 
     public DadosListagemLeilao(Leilao leilao) {
-        this(leilao.getName(), leilao.getPrice(), leilao.getData());
+        this(leilao.getName(), leilao.getPrice(), leilao.getData(), leilao.getId(), leilao.getUsuario().getId());
     }
     
 }
