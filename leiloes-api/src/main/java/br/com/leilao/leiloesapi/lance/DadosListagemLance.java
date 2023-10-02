bb7dbc746810d142226bede3ebb549ab2f3fb56b
Larissa Cruz <larissacruz_01@hotmail.com>
2023-10-02 19:23:30 -0300

    alterando formato data no json no dto dados listagem lance

--- leiloes-api/src/main/java/br/com/leilao/leiloesapi/lance/DadosListagemLance.java
+++ leiloes-api/src/main/java/br/com/leilao/leiloesapi/lance/DadosListagemLance.java
@@ -2,7 +2,9 @@ package br.com.leilao.leiloesapi.lance;
 
 import java.time.LocalDate;
 
-public record DadosListagemLance(LocalDate data, String nomeUsuario, Double lance) {
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public record DadosListagemLance(@JsonFormat(pattern="dd-MM-yyyy") LocalDate data, String nomeUsuario, Double lance) {
 
     public DadosListagemLance(Lance lance) {
         this(LocalDate.now(), lance.getLeilao().getUsuario().getName(), lance.getLance());
