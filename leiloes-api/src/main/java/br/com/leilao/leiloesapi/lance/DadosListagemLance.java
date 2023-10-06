625088ec425832fba00fdeed4cec57960b9fc7b7
Larissa Cruz <larissacruz_01@hotmail.com>
2023-10-06 12:01:16 -0300

    alterando dados listagem lance

--- leiloes-api/src/main/java/br/com/leilao/leiloesapi/lance/DadosListagemLance.java
+++ leiloes-api/src/main/java/br/com/leilao/leiloesapi/lance/DadosListagemLance.java
@@ -1,13 +1,14 @@
 package br.com.leilao.leiloesapi.lance;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-public record DadosListagemLance(@JsonFormat(pattern = "dd/MM/yyyy") LocalDateTime dataLance, String nomeUsuario, Double lance) {
+public record DadosListagemLance(@JsonFormat(pattern = "dd/MM/yyyy") LocalDateTime dataLance, String nomeUsuario, Double lance, String nameleilao, String donoleilao, @JsonFormat(pattern = "dd/MM/yyyy") LocalDate dataleilao) {
 
     public DadosListagemLance(Lance lance) {
-        this(lance.getDataLance(), lance.getUsuario().getName(), lance.getLance());
+        this(lance.getDataLance(), lance.getUsuario().getName(), lance.getLance(), lance.getLeilao().getName(), lance.getLeilao().getUsuario().getName(), lance.getLeilao().getData());
     }
     
 }
