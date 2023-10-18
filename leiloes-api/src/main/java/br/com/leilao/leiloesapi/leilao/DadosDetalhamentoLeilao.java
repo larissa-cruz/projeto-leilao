a018e5fcc1fef5bc800ddfb7cbec4f3f206065bd
Larissa Cruz <larissacruz_01@hotmail.com>
2023-10-17 21:24:00 -0300

    alterando Dados Detalhamento Leilao

--- leiloes-api/src/main/java/br/com/leilao/leiloesapi/leilao/DadosDetalhamentoLeilao.java
+++ leiloes-api/src/main/java/br/com/leilao/leiloesapi/leilao/DadosDetalhamentoLeilao.java
@@ -7,11 +7,11 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import br.com.leilao.leiloesapi.lance.Lance;
 
-public record DadosDetalhamentoLeilao(Long idleilao, Long iduser, String name, Double price, @JsonFormat(pattern = "dd/MM/yyyy") LocalDate data, List<Lance> lances) {
+public record DadosDetalhamentoLeilao(Long idleilao, Long iduser, String nameUser, String name, Double price, @JsonFormat(pattern = "dd/MM/yyyy") LocalDate data, List<Lance> lances) {
 
     public DadosDetalhamentoLeilao(Leilao leilao) {
 
-    this(leilao.getId(), leilao.getUsuario().getId(), leilao.getName(), leilao.getPrice(), leilao.getData(), leilao.getLances());
+    this(leilao.getId(), leilao.getUsuario().getId(), leilao.getUsuario().getName(), leilao.getName(), leilao.getPrice(), leilao.getData(), leilao.getLances());
 
     }
 
