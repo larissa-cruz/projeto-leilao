eb30294d516e1160d32002916ab60b5207932279
Larissa Cruz <larissacruz_01@hotmail.com>
2024-03-16 20:10:19 -0300

    alterando UsuarioLoginDTO

--- leiloes-api/src/main/java/br/com/leilao/leiloesapi/dtos/UsuarioLoginDTO.java
+++ leiloes-api/src/main/java/br/com/leilao/leiloesapi/dtos/UsuarioLoginDTO.java
@@ -5,10 +5,11 @@ import jakarta.validation.constraints.NotBlank;
 
 public record UsuarioLoginDTO(
         Long id,
+        String name,
         @NotBlank(message = "Não pode ser vazio") String username,
         @NotBlank(message = "Não pode ser vazio") String password
 ) {
     public UsuarioLoginDTO(Usuario usuario) {
-        this(usuario.getId(), usuario.getUsername(), usuario.getPassword());
+        this(usuario.getId(), usuario.getName(), usuario.getUsername(), usuario.getPassword());
     }
 }
